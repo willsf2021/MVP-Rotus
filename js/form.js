b1f8591ce75dc9200c4bb52580b18fc78d6e825a
Wilson Junior <wilson.junior@mxdrive.com.br>
2025-10-12 00:10:53 -0300

    feat: redirect logic with mocked data

--- js/form.js
+++ js/form.js
@@ -9,3 +9,48 @@ document.querySelectorAll('.password-field').forEach(field => {
         toggle.classList.toggle('bi-eye-slash', isPassword);
     });
 });
+
+let submitButton = document.querySelector(".submit-button")
+
+submitButton.addEventListener('click', (e) => {
+    submitForm(e)
+})
+
+function submitForm(e) {
+
+    page = e.target.dataset.page;
+
+    let alertHeading = page == 'login' ? 'Validado' : 'Cadastrado'
+    let alertMessage = page == 'login' ? 'Redirecionando para página segura...' : 'Redirecionando para página de autenticação...';
+
+    let section = document.querySelector('.section-form');
+    section.innerHTML = '';
+
+    let alertContainerEl = document.createElement('div')
+    alertContainerEl.classList.add('alert_container')
+
+    alertContainerEl.innerHTML = `
+        <img src="./assets/images/success_action.svg" alt="">
+        <h4>${alertHeading}</h4>
+        <p>${alertMessage}</p>
+                                `
+
+    section.appendChild(alertContainerEl);
+    redirectTo(alertContainerEl, page)
+
+}
+
+
+function redirectTo(alertContainerEl, page) {
+    setTimeout(() => {
+        alertContainerEl.classList.add('visibility')
+    }, 50)
+    setTimeout(() => {
+        alertContainerEl.classList.remove('hidden')
+        if (page == 'login') {
+            window.location.href = "/home.html";
+        } else if (page = 'register') {
+            window.location.href = "/login.html";
+        }
+    }, 5000)
+}
